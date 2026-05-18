172b54cd47468ea3b21a5118ffa8ff01ed30ee5f
Tarpan <tarpansaikia@gmail.com>
2026-05-19 00:05:22 +0530

    Added background crash detection and emergency SOS system

--- android/app/src/main/java/com/example/roadsos/MainActivity.kt
+++ android/app/src/main/java/com/example/roadsos/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.roadsos
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,18 +12,73 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.roadsos.screens.auth.AuthScreen
 import com.example.roadsos.screens.navigation.MainContainerScreen
+import com.example.roadsos.screens.permissions.PermissionScreen
 import com.example.roadsos.screens.splash.SplashScreen
 import com.example.roadsos.theme.RoadSoSTheme
 import kotlinx.coroutines.delay
-import com.example.roadsos.screens.permissions.PermissionScreen
 
 class MainActivity : ComponentActivity() {
+    companion object {
+
+        var isAppOpen = false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+
+        // NOTIFICATION PERMISSION
+        // REQUIRED FOR ANDROID 13+
+
+        if (
+            Build.VERSION.SDK_INT >=
+            Build.VERSION_CODES.TIRAMISU
+        ) {
+
+            if (
+                ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.POST_NOTIFICATIONS
+                    ),
+                    101
+                )
+            }
+        }
+
+        // START CRASH DETECTION SERVICE
+
+        val serviceIntent =
+            Intent(
+                this,
+                CrashDetectionService::class.java
+            )
+
+        if (
+            Build.VERSION.SDK_INT >=
+            Build.VERSION_CODES.O
+        ) {
+
+            startForegroundService(
+                serviceIntent
+            )
+
+        } else {
+
+            startService(
+                serviceIntent
+            )
+        }
 
         setContent {
 
@@ -27,7 +86,9 @@ class MainActivity : ComponentActivity() {
 
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+
+                    color =
+                        MaterialTheme.colorScheme.background
                 ) {
 
                     RoadSoSApp()
@@ -35,22 +96,40 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+    override fun onStart() {
+
+        super.onStart()
+
+        isAppOpen = true
+    }
+
+    override fun onStop() {
+
+        super.onStop()
+
+        isAppOpen = false
+    }
 }
 
 @Composable
 fun RoadSoSApp() {
 
     var showSplash by remember {
+
         mutableStateOf(true)
     }
 
     var isLoggedIn by remember {
+
         mutableStateOf(false)
     }
 
     var showPermissions by remember {
+
         mutableStateOf(false)
     }
+
+    // SPLASH DELAY
 
     LaunchedEffect(Unit) {
 
@@ -61,14 +140,14 @@ fun RoadSoSApp() {
 
     when {
 
-        // SPLASH
+        // SPLASH SCREEN
 
         showSplash -> {
 
             SplashScreen()
         }
 
-        // PERMISSION FLOW AFTER SIGNUP
+        // PERMISSION SCREEN
 
         showPermissions -> {
 
